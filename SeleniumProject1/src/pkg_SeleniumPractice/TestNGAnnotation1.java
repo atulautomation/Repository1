ca11c1a71a5d6ca51b989220438ec76ff0eb3a0a
Atul <atulingle05@gmail.com>
2022-08-21 18:08:25 +0530

    This is first commit

---
+++ SeleniumProject1/src/pkg_SeleniumPractice/TestNGAnnotation1.java
@@ -0,0 +1,24 @@
+package pkg_SeleniumPractice;
+
+import org.testng.annotations.Test;
+
+public class TestNGAnnotation1 {
+
+	@Test
+	public void TC_01()
+	{
+		System.out.println("This is first selenium test");
+	}
+	
+	@Test
+	public void TC_02()
+	{
+		System.out.println("This is second selenium test");
+	}
+	
+	@Test
+	public void TC_03()
+	{
+		System.out.println("This is third selenium test");
+	}
+}
