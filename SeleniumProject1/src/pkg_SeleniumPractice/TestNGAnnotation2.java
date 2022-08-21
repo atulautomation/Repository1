82112b0095bebaed7f65c90f02fcc715262703e6
Atul <atulingle05@gmail.com>
2022-08-21 18:18:45 +0530

    This is second commit

---
+++ SeleniumProject1/src/pkg_SeleniumPractice/TestNGAnnotation2.java
@@ -0,0 +1,24 @@
+package pkg_SeleniumPractice;
+
+import org.testng.annotations.Test;
+
+public class TestNGAnnotation2 {
+
+	@Test
+	public void TC_01()
+	{
+		System.out.println("This is first RESTAPI test");
+	}
+	
+	@Test
+	public void TC_02()
+	{
+		System.out.println("This is second RESTAPI test");
+	}
+	
+	@Test
+	public void TC_03()
+	{
+		System.out.println("This is third RESTAPI test");
+	}
+}
